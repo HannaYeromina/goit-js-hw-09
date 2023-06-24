7873136704a5ea3ec9578336e0fed35665da48b1
Anna <anna@Annas-MacBook-Air.local>
2023-06-24 11:59:10 -0700

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyColor = document.querySelector('body');
+let timerId = null;
 
+function getRandomHexColor() {
+     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+   }
+
+startBtn.addEventListener("click",() => {
+    timerId = setInterval(() => {
+        bodyColor.style.backgroundColor = `${getRandomHexColor()}`;
+        startBtn.disabled = true;
+        stopBtn.disabled = false;
+    }, 1000);
+  });
+
+stopBtn.addEventListener("click",() => {
+    clearInterval(timerId);
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+  });
+
+
+  
