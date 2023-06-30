7d208406840fb2d5df9f2a827323b022eeb25e86
HannaYeromina <113713051+HannaYeromina@users.noreply.github.com>
2023-06-30 19:20:08 +0000

    Deploying to gh-pages from @ HannaYeromina/goit-js-hw-09@ae5c18012fa5eeb3ab9f472d9190051b4c0f998f 🚀

--- 03-promises.16e2f9cc.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form").addEventListener("submit",(function(e){e.preventDefault(),console.log(delay.value),console.log(step.value),console.log(amount.value),console.log(hi)}));
-//# sourceMappingURL=03-promises.16e2f9cc.js.map
